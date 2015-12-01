683e6a99cc55c3ceab58cc2a329479b3a4e72a63
Emily Manders <manders.emily@gmail.com>
2015-11-30 23:17:21 -0800

    [index] Added form, js validation for tracking a single UPS shipment

---
+++ app/static/validator.js
@@ -0,0 +1,50 @@
+var GP = GP || {};
+
+var Validator = (function() {
+  var $trackButton = $('#cover-button');
+
+  function addTrackButtonClickListener() {
+    $trackButton.click(handleTrackBtnClick);
+  }
+
+  function handleTrackBtnClick(event) {
+    event.preventDefault();
+    validateFormInput();
+  }
+
+  function validateFormInput() {
+    var $formInput = $('#cover-input').val().trim();
+    var emptyError = 'Please enter a tracking number.';
+    var invalidTrackingNumber = 'Please enter a valid UPS tracking number. Ex: 1Z2W99W3PP20791577';
+    var errorMessage = false;
+    
+    $('#error-message').empty();
+
+    if ($formInput === '') {
+      errorMessage = emptyError;
+    } else if (!/1Z[A-Z0-9]{16}/.test($formInput)) {
+      errorMessage = invalidTrackingNumber;
+    }
+
+    if (errorMessage) {
+      $('#tagline').css('margin-bottom', '10px');
+      $('#error-message').html(errorMessage).removeClass('hidden');
+    } else {
+      $('#tagline').css('margin-bottom', '70px');
+      $('#error-message').addClass('hidden');
+      alert('no errors!');
+    }
+  }
+
+  function init() {
+    addTrackButtonClickListener();
+  }
+
+  return {
+    init: init
+  };
+})();
+
+$(document).ready(function () {
+  GP.validator = Validator.init();
+});
